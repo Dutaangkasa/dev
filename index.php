ed5df0fa8634fb11fbf227e9b1379b6b14bd62f7
Dutaangkasa <angkasawiguna@yahoo.com>
2025-01-02 22:47:13 +0700

    kode slicing

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('head.php') ?>
+</head>
+
+<body>
+    <?php include('navbar.php') ?>
+    <?php include('home.php') ?>
+    <?php include('footer.php') ?>
+    <?php include('header.php') ?>
+    <?php include('content.php') ?>
+</body>
+</html>
